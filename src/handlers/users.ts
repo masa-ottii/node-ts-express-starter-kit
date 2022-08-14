062b688dad85df97637a5ce966254cffb396b7ef
ochi@casareal.co.jp <ochi@casareal.co.jp>
2022-08-14 22:50:55 +0900

    package update

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,42 +1,34 @@
-import { NextFunction, Request, Response } from "express";
-import CreateError from "http-errors";
+import { NextFunction, Request, Response } from 'express';
+import CreateError from 'http-errors';
 
-import getConnection from "../util/connection";
+import getConnection from '../util/connection';
 
-export const getUsers = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
   const result = [
-    { id: 1, naiyo: "プログラミングする!", kinkyu: "すぐ" },
-    { id: 2, naiyo: "テストする", kinkyu: "気が向いたら" },
-    { id: 3, naiyo: "納品する", kinkyu: "そのうち" },
+    { id: 1, user_id: 'TANAKA_001', password: 'hogehoge' },
+    { id: 2, user_id: 'YAMADA_002', password: 'piyopiyo' },
+    { id: 3, user_id: 'SAITOU_003', password: 'foobar' },
   ];
-  res.status(200).json({ items: result });
+  res.status(200).json(result);
 };
 
-export const getUserById = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
   // パスパラメータの取得
   const userId = req.params.user_id;
   // パスパラメータの設定がない場合はエラーとして終了
   if (!userId) {
-    next(CreateError(400, "not found path parameter => user_id"));
+    next(CreateError(400, 'not found path parameter => user_id'));
   }
   let connection: any;
   let result: any;
   try {
     connection = await getConnection();
-    const selectSql = "SELECT id,naiyo,kinkyu FROM users WHERE id=?";
+    const selectSql = 'SELECT id,user_id,password FROM users WHERE id=?';
     // SQLの実行
     result = await connection.execute(selectSql, [userId]);
     if (result[0].length === 0) {
       // 該当データが無い場合
-      next(CreateError(404, "not found data id:" + userId));
+      next(CreateError(404, 'not found data id:' + userId));
     } else {
       // 該当データがある場合
       res.status(200).json(result[0][0]);
@@ -49,35 +41,32 @@ export const getUserById = async (
   }
 };
 
-export const addUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const addUser = async (req: Request, res: Response, next: NextFunction) => {
   // POSTデータの取得
-  const id = req.body.id;
-  const naiyo = req.body.naiyo;
-  const kinkyu = req.body.kinkyu;
+  const userId = req.body.user_id;
+  const password = req.body.password;
   // POSTデータがない場合はエラーとして終了
-  if (!id || !naiyo || !kinkyu) {
-    next(CreateError(400, "not found body data"));
+  if (!userId || !password) {
+    next(CreateError(400, 'illegal request body data'));
+    return;
   }
   let connection: any;
   let result: any;
   try {
     connection = await getConnection();
-    const insertSql = "INSERT INTO users(id,naiyo,kinkyu) VALUES (?,?,?)";
+    const insertSql = 'INSERT INTO users(user_id,password) VALUES (?,?)';
     // SQLの実行
-    result = await connection.execute(insertSql, [id, naiyo, kinkyu]);
+    result = await connection.execute(insertSql, [userId, password]);
+    res.status(201).json(result[0][0]);
   } catch (err) {
     next(err);
   } finally {
     // MySQLへの接続を終了
     if (connection) await connection.end();
   }
-  res.status(201).json(result[0][0]);
 };
 
 export const updateUser = (req: Request, res: Response) => {
-  res.status(200).json({ id: 10, naiyo: "基本設計をする", kinkyu: "なるはや" });
+  //未実装のため404をレスポンス
+  res.status(404).end();
 };
